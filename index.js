ed4e5e0ecc08eb2693308181cf321f109df442cb
Antonio Molinari <molinari@incode.it>
2020-06-07 00:01:27 +0200

    wip

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
 
+var fs = require("fs");
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+var publicdir = __dirname + "/public";
+app.use(function (req, res, next) {
+  if (req.path.indexOf(".") === -1) {
+    var file = publicdir + req.path + ".html";
+    fs.exists(file, function (exists) {
+      if (exists) req.url += ".html";
+      next();
+    });
+  } else next();
+});
+app.use(express.static(publicdir));
 
 app.get("/settings", function (req, res) {
   var data = {
@@ -11,7 +22,7 @@ app.get("/settings", function (req, res) {
     "mqtt_port": process.env.MQTT_PORT,
     "mqtt_username": process.env.MQTT_USERNAME,
     "mqtt_password": process.env.MQTT_PASSWORD,
-    "mqtt_destinationName": process.env.MQTT_DESTINATION_NAME
+    "mqtt_topic": process.env.MQTT_TOPIC
   }
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(data));
--- public/scripts.js
+++ public/scripts.js
@@ -1,12 +1,4 @@
 
-var settings = {};
-var mqtt_client = null;
-var default_message_interval = null;
-
-$(function () {
-  init();
-})
-
 function init() {
   $.getJSON('./settings', function (data) {
     settings = data;
@@ -15,9 +7,10 @@ function init() {
     $('#input-mqtt_port').val(settings.mqtt_port);
     $('#input-mqtt_username').val(settings.mqtt_username);
     $('#input-mqtt_password').val(settings.mqtt_password);
-    $("#input-mqtt_destinationName").val(settings.mqtt_destinationName);
+    $("#input-mqtt_topic").val(settings.mqtt_topic);
 
     $("form#form-mqtt-server").on("submit", function (e) {
+      $.LoadingOverlay("show");
       e.preventDefault();
       updateSettings();
       connect();
@@ -30,13 +23,19 @@ function init() {
 
     default_message_interval = setInterval(function(){
       var m = "message sent at "+moment().format();
-      $("#input-messageString").val(m);
+      $("#input-message-string").val(m);
     },1000)
     
-    $("#input-messageString").on("focus", function(){
+    $("#input-message-string").on("focus", function(){
       clearInterval(default_message_interval);
     })
+
+    $("#reset-messages").on("click", function(e){
+      e.preventDefault();
+      $("#input-messages").val("")
+    });
     
+    printMessage();
     
   });
 }
@@ -51,6 +50,7 @@ function updateSettings(){
 
 function connect(){
   //https://www.cloudmqtt.com/docs/websocket.html
+  
 
   // Create a mqtt_client instance
   mqtt_client = new Paho.MQTT.Client(settings.mqtt_server, 36246, "web_" + parseInt(Math.random() * 100, 10));
@@ -59,13 +59,14 @@ function connect(){
   // set callback handlers
   mqtt_client.onConnectionLost = onConnectionLost;
   mqtt_client.onMessageArrived = onMessageArrived;
+  mqtt_client.onMessageDelivered = onMessageDelivered;
   var options = {
     useSSL: true,
     userName: settings.mqtt_username,
     password: settings.mqtt_password,
     onSuccess: onConnect,
     onFailure: doFail
-  }
+  };
 
   // connect the mqtt_client
   mqtt_client.connect(options);
@@ -73,40 +74,90 @@ function connect(){
 
 // called when the mqtt_client connects
 function onConnect() {
+  $.LoadingOverlay("hide");
   // Once a connection has been made, make a subscription and send a message.
   console.log("onConnect");
-  // mqtt_client.subscribe(settings.mqtt_destinationName);
+  // mqtt_client.subscribe(settings.mqtt_topic);
   // message = new Paho.MQTT.Message("Hello CloudMQTT");
-  // message.destinationName = settings.mqtt_destinationName;
+  // message.topic = settings.mqtt_topic;
   // mqtt_client.send(message);
+  connected = true;
+  $("#submit-message").attr("disabled",false);
   sendMessage()
-
-  $("#badge-mqtt-serber-status").text("connected").addClass("badge-success").removeClass("badge-secondary");
+  $("#badge-mqtt-server-status").text("connected").addClass("badge-success").removeClass("badge-secondary");
 }
 
 
 function doFail(e) {
+  showModal(e.message, "doFail");
   console.log(e);
 }
 
 // called when the mqtt_client loses its connection
 function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0) {
+    showModal(responseObject.errorMessage, "onConnectionLost", true);
     console.log("onConnectionLost:" + responseObject.errorMessage);
   }
+}
+
+function onMessageDelivered(message){
+  console.log("onMessageDelivered:" + message.payloadString);
+  $.LoadingOverlay("hide");
 }
 
 // called when a message arrives
 function onMessageArrived(message) {
   console.log("onMessageArrived:" + message.payloadString);
+  printMessage(message.payloadString);
 }
 
 
-function sendMessage(destinationName, message){
-  var destinationName = $('#input-mqtt_destinationName').val();
-  var messageString = $('#input-messageString').val();
-  mqtt_client.subscribe(destinationName);
-  message = new Paho.MQTT.Message(messageString);
-  message.destinationName = destinationName;
-  mqtt_client.send(message);
+function sendMessage(topic, message){
+  $.LoadingOverlay("show");
+  var topic = $('#input-mqtt_topic').val();
+  var string = $('#input-message-string').val();
+  var qos = parseInt($("#input-message-qos").val());
+  var retained = $("#input-message-retain").is(":checked");
+  mqtt_client.subscribe(topic);
+  message = new Paho.MQTT.Message(string);
+  message.destinationName = topic;
+  //mqtt_client.send(message); //https://www.eclipse.org/paho/files/jsdoc/Paho.MQTT.Client.html
+  console.log('sending message: "'+string+'", topic: '+topic+', qos: '+qos+', retained: '+retained);
+  mqtt_client.send(topic, string, qos, retained);
 }
+
+
+function showModal(content, title, isCode) {
+  if(title == "undefined") title = "Message";
+  if(isCode == "undefined") isCode = false;
+  $("#modalDialogLabel").text(title);
+  $.LoadingOverlay("hide");
+  var modalContent = isCode ? "<pre>" + content + "</pre>" : content;
+  $("#modalDialog .modal-body").html(modalContent);
+  var options = {};
+  $("#modalDialog").modal(options);
+} 
+
+function printMessage(messageString){
+  var count = 0;
+  if (messageString == "undefined") messageString = false;
+  if (messageString){
+    var rowSeparator = "\r";
+    var content = $("#input-messages").text().trim();
+    content = messageString + rowSeparator + content;
+    $("#input-messages").text(content);
+    count = content.split(rowSeparator).length;
+  }
+  $("#messagesInfo").text("count: " + count);
+}
+
+
+var connected = false;
+var settings = {};
+var mqtt_client = null;
+var default_message_interval = null;
+
+$(function () {
+  init();
+});
